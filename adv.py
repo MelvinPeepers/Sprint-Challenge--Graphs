f44a62e8ca7b5c6b04df68a3e48290e12eb844f3
Melvin <peepersm@gmail.com>
2020-05-03 23:46:59 -0400

    removed prints

--- adv.py
+++ adv.py
@@ -14,8 +14,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -28,6 +28,7 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+traversal_path = []
 
 
 class Stack():
@@ -53,17 +54,52 @@ class Stack():
         return len(self.stack)
 
 
-traversal_path = []
-
-
 def dft(self, traversal_path):
+    # Create a ss  LIFO
+    plan_to_visit = Stack()
+    # push starting room as you start in room 0
+    plan_to_visit.push(0)
+    # Create a set of traversed vertices
+    visited_vertices = set()
+    # while visited_vertices is less then the total number of rooms
+    while len(visited_vertices) < len(room_graph):
+        # current = to starting room
+        current_vertex = plan_to_visit.stack[-1]
+        # storage for rooms not visited
+        unvisited_room = []
+        # add the visited to stack
+        visited_vertices.add(current_vertex)
+        # get exits
+        neighbor = room_graph[current_vertex][1]
+        # loop through each heading, room of the items in neighbor return a list of tuple pairs
+        for heading, room in neighbor.items():
+            # if unvisited we don't want to revisit vertices that have already been checked
+            if room not in visited_vertices:
+                # add unvisited_rooms to visited
+                unvisited_room.append((room, heading))
+
+        # if unvisited room
+        if len(unvisited_room) > 0:
+            # add stack nested index
+            plan_to_visit.push(unvisited_room[0][0])
+            # add traversal_path nested index
+            traversal_path.append(unvisited_room[0][1])
+
+        else:
+            # remove from stack
+            plan_to_visit.pop()
+            # loop through each heading, room of the items in neighbor return a list of tuple pairs
+            for heading, room in neighbor.items():
+                if room == plan_to_visit.stack[-1]:
+                    # go to last room you were in
+                    traversal_path.append(heading)
 
     # Create a ss
-    #ss = Stack()
+    # ss = Stack()
     # push starting room as you start in room 0
     # ss.push(0)
     # Create a set of traversed vertices
-    #visited = set()
+    # visited = set()
     # While queue is not empty:
     # while ss.size() > 0:
     #    path = ss.pop()
@@ -102,15 +138,15 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-#######
+######
 # UNCOMMENT TO WALK AROUND
-#######
-# player.current_room.print_room_description(player)
-# while True:
-#    cmds = input("-> ").lower().split(" ")
-#    if cmds[0] in ["n", "s", "e", "w"]:
-#        player.travel(cmds[0], True)
-#    elif cmds[0] == "q":
-#        break
-#    else:
-#        print("I did not understand that command.")
+######
+player.current_room.print_room_description(player)
+while True:
+    cmds = input("-> ").lower().split(" ")
+    if cmds[0] in ["n", "s", "e", "w"]:
+        player.travel(cmds[0], True)
+    elif cmds[0] == "q":
+        break
+    else:
+        print("I did not understand that command.")
